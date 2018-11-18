08e477ada13a6b69cc0109c09abda76ae52dff77
GermanDZ <germ@ndz.com.ar>
2018-11-18 18:59:54 +0100

    add moving to direction to Position module

--- lib/position.ex
+++ lib/position.ex
@@ -5,5 +5,10 @@ defmodule Position do
 
   def build(_), do: {:error, "invalid position"}
 
+  def move(position, %Direction{value: :north}), do: %{position | y: position.y + 1}
+  def move(position, %Direction{value: :east}), do: %{position | x: position.x + 1}
+  def move(position, %Direction{value: :south}), do: %{position | y: position.y - 1}
+  def move(position, %Direction{value: :west}), do: %{position | x: position.x - 1}
+
   defp build_safe(x, y), do: {:ok, %Position{x: x, y: y}}
 end
--- test/position_test.exs
+++ test/position_test.exs
@@ -1,23 +1,55 @@
 defmodule PositionTest do
   use ExUnit.Case
 
-  test "build/1 returns {:ok, position} for {-1, -1}" do
-    assert {:ok, %Position{x: -1, y: -1}} = Position.build({-1, -1})
+  describe "creation" do
+    test "build/1 returns {:ok, position} for {-1, -1}" do
+      assert {:ok, %Position{x: -1, y: -1}} = Position.build({-1, -1})
+    end
+
+    test "build/1 returns {:ok, position} for {0, 0}" do
+      assert {:ok, %Position{x: 0, y: 0}} = Position.build({0, 0})
+    end
+
+    test "build/1 returns {:ok, position} for {1, 1}" do
+      assert {:ok, %Position{x: 1, y: 1}} = Position.build({1, 1})
+    end
+
+    test "build/1 returns {:error, 'invalid position'} elsewhere" do
+      assert {:error, "invalid position"} = Position.build(:no_dir)
+      assert {:error, "invalid position"} = Position.build({:center, 0})
+      assert {:error, "invalid position"} = Position.build("1, 2")
+      assert {:error, "invalid position"} = Position.build({0, :top})
+      assert {:error, "invalid position"} = Position.build({nil, nil})
+    end
   end
 
-  test "build/1 returns {:ok, position} for {0, 0}" do
-    assert {:ok, %Position{x: 0, y: 0}} = Position.build({0, 0})
-  end
+  describe "moving" do
+    test "to north" do
+      {:ok, position} = Position.build({0, 0})
+      {:ok, direction} = Direction.build(:north)
 
-  test "build/1 returns {:ok, position} for {1, 1}" do
-    assert {:ok, %Position{x: 1, y: 1}} = Position.build({1, 1})
-  end
+      assert %Position{x: 0, y: 1} == Position.move(position, direction)
+    end
 
-  test "build/1 returns {:error, 'invalid position'} elsewhere" do
-    assert {:error, "invalid position"} = Position.build(:no_dir)
-    assert {:error, "invalid position"} = Position.build({:center, 0})
-    assert {:error, "invalid position"} = Position.build("1, 2")
-    assert {:error, "invalid position"} = Position.build({0, :top})
-    assert {:error, "invalid position"} = Position.build({nil, nil})
+    test "to east" do
+      {:ok, position} = Position.build({3, -3})
+      {:ok, direction} = Direction.build(:east)
+
+      assert %Position{x: 4, y: -3} == Position.move(position, direction)
+    end
+
+    test "to south" do
+      {:ok, position} = Position.build({4, -2})
+      {:ok, direction} = Direction.build(:south)
+
+      assert %Position{x: 4, y: -3} == Position.move(position, direction)
+    end
+
+    test "to west" do
+      {:ok, position} = Position.build({-3, -2})
+      {:ok, direction} = Direction.build(:west)
+
+      assert %Position{x: -4, y: -2} == Position.move(position, direction)
+    end
   end
 end
