a019ad1965241c65a646cdec6c80f6c1c18199a4
GermanDZ <germ@ndz.com.ar>
2018-11-18 18:42:57 +0100

    add turning left & right functions to Direction module

--- lib/direction.ex
+++ lib/direction.ex
@@ -8,5 +8,15 @@ defmodule Direction do
 
   def build(_), do: {:error, "invalid direction"}
 
+  def turn_left(direction = %Direction{value: :north}), do: %{direction | value: :west}
+  def turn_left(direction = %Direction{value: :west}), do: %{direction | value: :south}
+  def turn_left(direction = %Direction{value: :south}), do: %{direction | value: :east}
+  def turn_left(direction = %Direction{value: :east}), do: %{direction | value: :north}
+
+  def turn_right(direction = %Direction{value: :north}), do: %{direction | value: :east}
+  def turn_right(direction = %Direction{value: :west}), do: %{direction | value: :north}
+  def turn_right(direction = %Direction{value: :south}), do: %{direction | value: :west}
+  def turn_right(direction = %Direction{value: :east}), do: %{direction | value: :south}
+
   defp build_safe(direction), do: {:ok, %Direction{value: direction}}
 end
--- test/direction_test.exs
+++ test/direction_test.exs
@@ -1,28 +1,74 @@
 defmodule DirectionTest do
   use ExUnit.Case
 
-  test "build/1 returns {:ok, direction} for :west" do
-    assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+  describe "creation" do
+    test "build/1 returns {:ok, direction} for :west" do
+      assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+    end
+
+    test "build/1 returns {:ok, direction} for :east" do
+      assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+    end
+
+    test "build/1 returns {:ok, direction} for :south" do
+      assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+    end
+
+    test "build/1 returns {:ok, direction} for :north" do
+      assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+    end
+
+    test "build/1 returns {:error, 'invalid direction'} elsewhere" do
+      assert {:error, "invalid direction"} = Direction.build(:no_west)
+      assert {:error, "invalid direction"} = Direction.build(:center)
+      assert {:error, "invalid direction"} = Direction.build("north")
+      assert {:error, "invalid direction"} = Direction.build(:n)
+      assert {:error, "invalid direction"} = Direction.build(0)
+      assert {:error, "invalid direction"} = Direction.build(nil)
+    end
   end
 
-  test "build/1 returns {:ok, direction} for :east" do
-    assert {:ok, %Direction{value: :west}} = Direction.build(:west)
+  describe "turning left" do
+    test "from north" do
+      {:ok, direction} = Direction.build(:north)
+      assert :west = Direction.turn_left(direction).value
+    end
+
+    test "from west" do
+      {:ok, direction} = Direction.build(:west)
+      assert :south = Direction.turn_left(direction).value
+    end
+
+    test "from south" do
+      {:ok, direction} = Direction.build(:south)
+      assert :east = Direction.turn_left(direction).value
+    end
+
+    test "from east" do
+      {:ok, direction} = Direction.build(:east)
+      assert :north = Direction.turn_left(direction).value
+    end
   end
 
-  test "build/1 returns {:ok, direction} for :south" do
-    assert {:ok, %Direction{value: :west}} = Direction.build(:west)
-  end
+  describe "turning right" do
+    test "from north" do
+      {:ok, direction} = Direction.build(:north)
+      assert :east = Direction.turn_right(direction).value
+    end
 
-  test "build/1 returns {:ok, direction} for :north" do
-    assert {:ok, %Direction{value: :west}} = Direction.build(:west)
-  end
+    test "from west" do
+      {:ok, direction} = Direction.build(:west)
+      assert :north = Direction.turn_right(direction).value
+    end
 
-  test "build/1 returns {:error, 'invalid direction'} elsewhere" do
-    assert {:error, "invalid direction"} = Direction.build(:no_west)
-    assert {:error, "invalid direction"} = Direction.build(:center)
-    assert {:error, "invalid direction"} = Direction.build("north")
-    assert {:error, "invalid direction"} = Direction.build(:n)
-    assert {:error, "invalid direction"} = Direction.build(0)
-    assert {:error, "invalid direction"} = Direction.build(nil)
+    test "from south" do
+      {:ok, direction} = Direction.build(:south)
+      assert :west = Direction.turn_right(direction).value
+    end
+
+    test "from east" do
+      {:ok, direction} = Direction.build(:east)
+      assert :south = Direction.turn_right(direction).value
+    end
   end
 end
