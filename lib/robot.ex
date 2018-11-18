2fc688c3d918c55d1eddc27be47bfee3c9c49f41
GermanDZ <germ@ndz.com.ar>
2018-11-18 19:03:24 +0100

    add move function to Robot module

--- lib/robot.ex
+++ lib/robot.ex
@@ -1,3 +1,15 @@
 defmodule Robot do
   defstruct ~w(position direction)a
+
+  def move(robot = %Robot{}, "L") do
+    %{robot | direction: Direction.turn_left(robot.direction)}
+  end
+
+  def move(robot = %Robot{}, "R") do
+    %{robot | direction: Direction.turn_right(robot.direction)}
+  end
+
+  def move(robot = %Robot{}, "A") do
+    %{robot | position: Position.move(robot.position, robot.direction)}
+  end
 end
--- test/robot_test.exs
+++ test/robot_test.exs
@@ -12,4 +12,54 @@ defmodule RobotTest do
       assert robot.direction == direction
     end
   end
+
+  describe "moving" do
+    test "move/2 for Left rotates from north to west" do
+      robot = %Robot{direction: %Direction{value: :north}, position: %Position{x: 0, y: 0}}
+
+      assert :west = Robot.move(robot, "L").direction.value
+    end
+
+    test "move/2 for Left rotates from east to north" do
+      robot = %Robot{direction: %Direction{value: :east}, position: %Position{x: 0, y: 0}}
+
+      assert :north = Robot.move(robot, "L").direction.value
+    end
+
+    test "move/2 for Right rotates from north to east" do
+      robot = %Robot{direction: %Direction{value: :north}, position: %Position{x: 0, y: 0}}
+
+      assert :east = Robot.move(robot, "R").direction.value
+    end
+
+    test "move/2 for Right rotates from west to north" do
+      robot = %Robot{direction: %Direction{value: :west}, position: %Position{x: 0, y: 0}}
+
+      assert :north = Robot.move(robot, "R").direction.value
+    end
+
+    test "move/2 for Advance increments y when is going north" do
+      robot = %Robot{direction: %Direction{value: :north}, position: %Position{x: 0, y: 0}}
+
+      assert %Position{x: 0, y: 1} = Robot.move(robot, "A").position
+    end
+
+    test "move/2 for Advance increments x when is going east" do
+      robot = %Robot{direction: %Direction{value: :east}, position: %Position{x: -3, y: 0}}
+
+      assert %Position{x: -2, y: 0} = Robot.move(robot, "A").position
+    end
+
+    test "move/2 for Advance decrements y when is going south" do
+      robot = %Robot{direction: %Direction{value: :south}, position: %Position{x: 0, y: 5}}
+
+      assert %Position{x: 0, y: 4} = Robot.move(robot, "A").position
+    end
+
+    test "move/2 for Advance decrements x when is going west" do
+      robot = %Robot{direction: %Direction{value: :west}, position: %Position{x: -3, y: 0}}
+
+      assert %Position{x: -4, y: 0} = Robot.move(robot, "A").position
+    end
+  end
 end
